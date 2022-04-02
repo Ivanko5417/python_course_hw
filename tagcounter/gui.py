319c86cf6b24f109dccef4d75b4d10905c0d73d1
Ivanko5417 <v.drobysh2011@gmail.com>
2022-04-02 15:53:11 +0400

    feat: add status bar

--- tagcounter/gui.py
+++ tagcounter/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 
 
 def __prettify_dict__(dictionary):
@@ -18,31 +19,46 @@ class GUI:
         get_btn = Button(window, text='Load', command=self.on_load_click)
         view_btn = Button(window, text='View', command=self.on_view_click)
         site_input = Entry(width=100)
-        status_label = Text(height=15, wrap=WORD)
+        result_input = Text(height=15, wrap=WORD)
+        status_label_text = StringVar()
+        status_label = Label(height=15, textvariable=status_label_text)
+
         self.__site_input__ = site_input
-        self.__status_label__ = status_label
+        self.__result_input__ = result_input
+        self.__status_label_text__ = status_label_text
 
         site_input.pack()
         get_btn.pack()
         view_btn.pack()
+        result_input.pack()
         status_label.pack()
 
         window.mainloop()
 
     def put_dict_to_text(self, dictionary):
-        self.__status_label__.delete('1.0', END)
-        self.__status_label__.insert(INSERT, __prettify_dict__(dictionary))
+        self.__result_input__.delete('1.0', END)
+        self.__result_input__.insert(INSERT, __prettify_dict__(dictionary))
 
     def on_load_click(self):
+        start_time = time.time()
         url_or_alias = self.__site_input__.get()
-        dictionary = self.__get_url_data__(url_or_alias)
-        self.put_dict_to_text(dictionary)
+        try:
+            dictionary = self.__get_url_data__(url_or_alias)
+            self.put_dict_to_text(dictionary)
+            self.__status_label_text__.set(f'Loading finished successfully. Execution time = {round(time.time() - start_time, 2)} seconds')
+        except BaseException:
+            self.__status_label_text__.set('Error occurred during loading')
 
     def on_view_click(self):
         url_or_alias = self.__site_input__.get()
-        url_data = self.__view_url_data__(url_or_alias)
-        if url_data is None:
-            self.__status_label__.delete('1.0', END)
-            self.__status_label__.insert(INSERT, 'Nothing found')
-            return
-        self.put_dict_to_text(url_data[3])
+        try:
+            url_data = self.__view_url_data__(url_or_alias)
+            if url_data is None:
+                self.__result_input__.delete('1.0', END)
+                self.__status_label_text__.set('Nothing found')
+                return
+            self.put_dict_to_text(url_data[3])
+            self.__status_label_text__.set('Viewing finished successfully')
+        except BaseException:
+            self.__status_label_text__.set('Error occurred viewing loading')
+
